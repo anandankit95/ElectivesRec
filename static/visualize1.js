5fcd26852a4bb8ad216ba18aac8a3eb42735ca4c
Anuj Hegde <anuj04hegde@gmail.com>
2018-11-22 00:20:13 +0530

    with chart

--- static/visualize1.js
+++ static/visualize1.js
@@ -34,9 +34,55 @@ $(document).ready(function() {
 		.done(function(data) {
 
 			$("#res").text(data);
+			var parsed = JSON.parse(data);
+			$(function () {
+					$('#container').highcharts({
+					chart: {	
+						    	renderTo : 'container',
+							plotBackgroundColor: null,
+							plotBorderWidth: null,
+							plotShadow: false,
+							type: 'pie'
+						    },
+						    title: {
+							text: 'Recommendation'
+						    },
+						    tooltip: {
+							pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+						    },
+						    plotOptions: {
+							pie: {
+							    allowPointSelect: true,
+							    cursor: 'pointer',
+							    dataLabels: {
+								enabled: true,
+								format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+								style: {
+								    color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+								}
+							    }
+							}
+						    },
+						    series: [{
+							name: 'Brands',
+							colorByPoint: true,
+							data: [{
+							    name: "subject1",
+							    y: 61.41
+							}, {
+							    name: 'subject2',
+							    y: 11.84
+							}, {
+							    name: 'subject3',
+							    y: 10.85
+							}, {
+							    name: 'subject4',
+							    y: 2.61
+							}]
+						    }]
 
-		});
-
+});
+});
 		event.preventDefault();
 
 	});
